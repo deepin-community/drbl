d46f8bbc370eb2f4cbcfbcafd19d1ce583a9aaba
xzl <xiangzelong@deepin.org>
2023-12-29 15:04:34 +0800

    feat: update drbl to 5.2.28
    diskless remote boot, and a disk cloning utility
    
    Issue: https://github.com/linuxdeepin/developer-center/issues/5718
    Log: update repo

---
+++ toolbox/make-deb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Steven Shiau <steven _at_ clonezilla org)
+
+# Settings
+PKG="drbl"
+
+set -e
+
+#
+VER="$(LC_ALL=C head -n 1 debian/changelog  | grep -i "^${PKG}" | grep -E -o "\(.*\)" | sed -r -e "s/\(//g" -e "s/\)//g" | cut -d"-" -f1)"
+[ -z "$VER" ] && echo "No version found in debian/changelog! Program terminated!"
+echo "VER: $VER"
+
+#
+TARBALL=${PKG}-${VER}.tar.xz
+TARBALL_ORIG=${PKG}_${VER}.orig.tar.xz
+
+# check
+[ ! -f "$TARBALL" ] && echo "Can NOT find file $TARBALL! Program Stop!!!" && exit 1
+
+# mkdir for build
+rm -rf debforge
+mkdir debforge
+(cd debforge; ln -fs ../$TARBALL $TARBALL_ORIG)
+tar -xJf $TARBALL -C debforge/
+cp -a debian debforge/$PKG-$VER/
+cd debforge/$PKG-$VER
+debuild
+rm -f $TARBALL_ORIG
